8a7e94ec8a3a682c162647e32338cc82ff7ebcaa
Sam Thomas <sgpthomas@gmail.com>
2015-11-15 21:49:48 -0800

    worked on optimizing coffeev2.py

--- coffeev2.py
+++ coffeev2.py
@@ -1,35 +1,44 @@
 from random import randint
 import time
 import math
+import sys
 
-coffee_sym = "."
+coffee_sym = " "
+marked_sym = "."
 cream_sym = "X"
 
 def gen_cup (rows, cols):
     container = []
     cream = rows*0.5 #fill the top half of the container with cream
+    cream_count = 0
+    cream_lim = (rows*cols)/2.
     for y in range (rows): #loop through all rows
         container.append ([])
         for x in range (cols): #loop through all columns
             #if y is less than cream, insert true
-            if y < cream:
-                container[y].append (True)
+            if y < cream and cream_count <= cream_lim:
+                container[y].append (2)
+                cream_count += 1
             else: #else if y is greater than cream, insert space
-                container[y].append (False)
+                container[y].append (0)
 
     return container
 
 def gen_homo_cup (rows, cols):
     option1 = []
     option2 = []
+    coffee_count = 0
     for y in range (rows): #loop through all rows
         option1.append ([])
         for x in range (cols): #loop through all columns
             #if y is less than cream, insert true
             if (x % 2) == (y % 2):
                 option1[y].append (True)
+                coffee_count += 1
             else: #else if y is greater than cream, insert space
                 option1[y].append (False)
+
+    coffee_count = 0
 
     for y in range (rows): #loop through all rows
         option2.append ([])
@@ -37,6 +46,7 @@ def gen_homo_cup (rows, cols):
             #if y is less than cream, insert true
             if (x % 2) != (y % 2):
                 option2[y].append (True)
+                coffee_count += 1
             else: #else if y is greater than cream, insert space
                 option2[y].append (False)
 
@@ -51,10 +61,15 @@ def random_swap (c):
     x = randint (1, len (c[0]) - 2)
     y = randint (1, len (c) - 2)
     s = randint (0, 7)
-    #while (should_swap (x, y, s, c) != True):
-    #    x = randint (1, len (c[0]) - 2)
-    #    y = randint (1, len (c) - 2)
-    #    s = randint (0, 7)
+
+    c = mark_swap (c)
+    while (c[y][x] != 1):
+        x = randint (1, len (c[0]) - 2)
+        y = randint (1, len (c) - 2)
+        print c[y][x]
+        print ("regenning rand")
+
+    print "finished"
 
     """
     /       \
@@ -81,24 +96,35 @@ def random_swap (c):
 
     return c
 
-def should_swap (x, y, s, c):
-    shiftx = 0
-    shifty = 0
-    #set y shift
-    if s < 3:
-        shifty = -1
-    elif s > 4:
-        shifty = 1
+#optimization
+def mark_swap (con):
+    for y in range (len (con)):
+        for x in range (len (con[0])):
+            xs_arr = [0] #xshift
+            ys_arr = [0] #yshift
+            val = con[y][x]
+            #if (y == 0) or (y == len (con) - 1) or (x == 0) or (x == len (con[0]) - 1):
+            if (y != 0): #top edge
+                ys_arr.append (-1)
 
-    #set x shift
-    if s in [0,3,5]:
-        shiftx = -1
-    elif s in [2,4,7]:
-        shiftx = 1
+            if (y != len (con) - 1): #bottom edge
+                ys_arr.append (1)
 
-    if (c[y][x] != c[y+shifty][x+shiftx]):
-        return True
-    return False
+            if (x != 0):
+                xs_arr.append (-1)
+
+            if (x != len (con[0]) - 1):
+                xs_arr.append (1)
+
+            if (val == 2): #if value is 2 (cream)
+                for ys in ys_arr:
+                    for xs in xs_arr:
+                        if (con[y+ys][x+xs] == 0):
+                            con[y+ys][x+xs] = 1
+
+
+
+    return con
 
 
 def print_container (cont):
@@ -112,8 +138,10 @@ def print_container (cont):
     for row in cont: #loop through rows
         print "|",
         for col in row: #loop through columns
-            if col:
+            if col == 2:
                 print cream_sym,
+            elif col == 1:
+                print marked_sym,
             else:
                 print coffee_sym,
         print "|"
@@ -124,16 +152,47 @@ def print_container (cont):
         print "=",
     print "|"
 
-
 def main ():
-    print ("Running Simulation\n"),
+    width = 3
+    height = 3
+    n = 60
+
+    if (len (sys.argv) > 1):
+        width, height, n = int (sys.argv[2]), int (sys.argv[3]), int (sys.argv[4])
+        if (sys.argv[1] == 'iter'):
+            for_iter (width, height, n)
+        elif (sys.argv[1] == 'time'):
+            for_time (width, height, n)
+
+    else:
+        for_iter (width, height, n)
+
+
+def for_iter (w, h, n):
+
+    print ("Running Simulation {} times".format (n))
     start = time.time ()
 
-    container = gen_cup (12, 20)
-    perfect1, perfect2 = gen_homo_cup (20, 20)
+    container = gen_cup (w, h)
+    #perfect1, perfect2 = gen_homo_cup (width, height)
+    last_time = 0
+
+    for a in range (n):
+        container = random_swap (container)
+
+    print_container (container)
+    print ("Finished " + str (n) + " iterations in " + str (time.time () - start) + "\n")
+    print ("Have a nice day?")
+
+def for_time (w, h, t):
+    print ("Running Simulation for {} seconds".format (t))
+    start = time.time ()
+
+    container = gen_cup (w, h)
+    #perfect1, perfect2 = gen_homo_cup (width, height)
     itera = 0
-    last_time = -1
-    while (time.time () - start < 120):
+    last_time = 0
+    while (time.time () - start < time_lim):
         container = random_swap (container)
         itera += 1
         current_time = math.floor (time.time () - start)
@@ -141,14 +200,39 @@ def main ():
             print (str (itera) + " iterations in " + str (current_time) + " seconds")
             last_time = current_time
 
-        if (container == perfect1 or container == perfect2):
-            print ("\nMatched!")
-            break
+        #if (container == perfect1 or container == perfect2):
+        #    print ("\nMatched!")
+        #    break
 
         #print_container (container);
+
+
     print_container (container)
     print ("Finished " + str (itera) + " iterations in " + str (time.time () - start) + "\n")
     print ("Have a nice day?")
 
+def test_speed ():
+    print "Starting Time Trial"
+    current_time = 0
+    times = 20
+
+    for n in range (times):
+        start = time.time ()
+        c = gen_cup (20, 20);
+        for a in range (200000):
+            container = random_swap (c)
+
+        print ("Trial {} finished in {} seconds".format (n, time.time () - start))
+        current_time += time.time () - start
+
+    print "Finished Time Trial"
+    print "Test took {} seconds".format (current_time)
+    print "Average trial took {} seconds".format (current_time / times)
+
 if __name__ == '__main__':
-    main ()
+    #c = gen_cup (20, 20)
+    #for a in range (20000):
+    #    c = random_swap (c)
+    #c = mark_swap (c)
+    #print_container (c)
+    test_speed ()
