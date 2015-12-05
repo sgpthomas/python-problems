0efb784e6db7149addc64655d2aed01274454c13
Sam Thomas <sgpthomas@gmail.com>
2015-12-04 22:23:10 -0800

    Added merge sort

--- Sort/graphic_sort.py
+++ Sort/graphic_sort.py
@@ -8,27 +8,28 @@ import sorter as s
 from math import floor
 
 WIDTH, HEIGHT = 1047, 770
-SIZE = 300
+SIZE = 256
 
 class Worker(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.should_kill = False
         self.array = gen_rand_array(SIZE)
+        #self.array = [8, 7, 0, 3, 4, 2, 6, 1]
         app.set_array(self.array)
-        
+
         self.vars = {'iterations':0, 'swaps':0}
         self.count = 0
-        
+
     def run(self):
         while not self.should_kill:
             if is_sorted(app.array):
                 print self.vars['iterations'], self.vars['swaps'], self.count
                 self.kill()
-            else: 
+            else:
                 self.count += 1
                 s.sort(self, app.array, app)
-            
+
     def kill(self):
         self.should_kill = True
 
@@ -39,7 +40,7 @@ class CairoArea(Gtk.DrawingArea):
         Gtk.DrawingArea.__init__(self)
         self.expand = True
         self.show_all()
-        
+
         #vars
         self.uplim = 10
         self.lowlim = 0
@@ -47,15 +48,15 @@ class CairoArea(Gtk.DrawingArea):
         else: self.array = None
         self.selected = -1
         self.text = []
-        
+
     def draw(self, widget, cr):
         if self.array != None:
             #establish drawing dimensions
             width = self.get_allocated_width()
             height = self.get_allocated_height()
-            bar_width = width / len(self.array)
-            quantize = floor(height / float(self.uplim))
-            
+            bar_width = float(width) / len(self.array)
+            quantize = height / float(self.uplim)
+
             for i in range(len(self.array)):
                 #print (self.array[i], quantize, height)
                 if i == self.selected:
@@ -69,7 +70,7 @@ class CairoArea(Gtk.DrawingArea):
                     cr.fill()
                 else:
                     cr.stroke()
-                    
+
             #text rendering
             cursor = 20
             for s in self.text:
@@ -79,50 +80,50 @@ class CairoArea(Gtk.DrawingArea):
                 cr.move_to(10, cursor)
                 cr.show_text(s)
                 cursor += 15
-        
+
     def set_array(self, array):
         high = None
         low = None
         for val in array:
             if high == None: high = val
             elif val > high: high = val
-            
+
             if low == None: low = val
             elif val < low: low = val
-        
+
         self.uplim = high
         self.lowlim = low
         self.array = array
-        
+
     #for when he high and the low of the array don't change
     def update_array(self, array):
         self.array = array
         self.update()
-        
+
     def select(self, index):
         self.selected = index
         self.update()
-        
+
     def update(self):
         self.queue_draw_area(0, 0, self.get_allocated_width(), self.get_allocated_height())
-        
+
 def gen_rand_array(length):
     ret_arr = []
     for a in range(length):
         ret_arr.append(randint(0, length))
-        
+
     return ret_arr
-    
+
 def is_sorted(array):
     for i in range(1, len(array)):
         if array[i] < array[i-1]:
             return False
     return True
-    
+
 def destroy(window):
     thr.kill()
     Gtk.main_quit()
-    
+
 app = CairoArea()
 thr = Worker()
 def main():
@@ -134,31 +135,13 @@ def main():
     box.set_border_width(10)
     box.pack_start(app, True, True, 0)
     window.add(box)
-    
+
     app.connect('draw', app.draw)
     window.connect_after('destroy', destroy)
     window.show_all()
-    
+
     thr.start()
     Gtk.main()
 
 if __name__ == "__main__":
     sys.exit(main())
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- Sort/sorter.py
+++ Sort/sorter.py
@@ -2,67 +2,70 @@ from random import randint
 from graphic_sort import CairoArea, Worker
 from time import sleep
 
+DELAY = 0.05
+
 def sort(w, array, ca):
     #label
     if len(ca.text) != 3:
-        ca.text = []   
+        ca.text = []
         ca.text.append("Graphic Sorting")
         ca.text.append("Iterations: 0")
         ca.text.append("Swaps: 0")
-        
-    better_bubblesort(w, array, ca)
-    #bubblesort(w, array, ca)
-       
+
+    merge_sort(w, array, ca)
+    #better_bubblesort(w, array, ca)
+
 def better_bubblesort(w, array, ca):
     #label
     ca.text[0] = "Better Bubble Sorting"
-    
+
     if 'bubble' not in w.vars: w.vars['bubble'] = len(array)
     l = w.vars['bubble']
     for i in range(1, l):
         w.vars['iterations'] += 1
-        sleep(0.001)
+        sleep(DELAY)
         ca.select(i)
         if array[i] < array[i-1]:
             w.vars['swaps'] += 1
             array[i], array[i-1] = array[i-1], array[i]
         ca.update_array(array)
-        
+
         ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
         ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
-        
+
     w.vars['bubble'] = l - 1
-    
+
 def bubblesort(w, array, ca):
     #label
     ca.text[0] = "Bubble Sorting"
-    
+
     for i in range(1, len(array)):
         w.vars['iterations'] += 1
-        sleep(0.0001)
+        sleep(DELAY)
         ca.select(i)
         if array[i] < array[i-1]:
             w.vars['swaps'] += 1
             array[i], array[i-1] = array[i-1], array[i]
         ca.update_array(array)
-        
+
         ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
         ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
-        
+
+#don't really know how this works. oh well.
 def sam_sort(w, array, ca):
     #label
     if len(ca.text) != 3:
-        ca.text = []   
+        ca.text = []
         ca.text.append("Sam's Sorting")
         ca.text.append("Iterations: 0")
         ca.text.append("Swaps: 0")
-        
+
     end_index = -1
     for i in range(0, len(array) + end_index):
         w.vars['iterations'] += 1
-        sleep(0.0001)
+        sleep(DELAY)
         ca.select(i)
-        
+
         insert = -1
         for c in range(end_index, insert+1):
             if array[i] < array[c]:
@@ -70,61 +73,104 @@ def sam_sort(w, array, ca):
         array.insert(insert, array.pop(0))
         ca.update_array(array)
         end_index -= 1
-        
+
         ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
         ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
-        
-def merge_sort (w, array, ca):
+
+def merge_sort(w, array, ca):
+    #print array
     #title
     ca.text[0] = "Merge Sort"
 
+    end_arr = []
+    #sort in groups of 2
+    for i in range(0, len(array), 2):
+        sleep(0.001)
+        ca.select(i)
+        w.vars['iterations'] += 1
+        if array[i] > array[i+1]: array[i], array[i+1] = swap(w, array[i], array[i+1])
+
+        ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
+        ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
+
+    #print array
+    ar_len = 2
+    #print "start"
+    while ar_len < (len(array)/2)+1:
+        #print "start 2"
+        for curr_in in range(0, len(array), ar_len*2):
+            #print (ar_len, curr_in)
+            ar, br = [], []
+            for a in range(ar_len):
+                #print curr_in + a, curr_in + a + ar_len
+                ar.append(array[curr_in + a])
+                br.append(array[curr_in + a + ar_len])
+            #print ar, br,
+
+            cr = merge_helper(ar, br, w, ca, curr_in)
+            #print cr
+            for i in range(len(cr)):
+                sleep(DELAY)
+                w.vars['swaps'] += 1
+                array[curr_in+i] = cr[i]
+                ca.select(curr_in+i)
+
+            ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
+            ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
+        #print "done 2"
+        ar_len *= 2
+    print "done"
+
+def merge_helper(a, b, w, ca, ci):
+    c = []
+    i = 0
+    while len(a) > 0 or len(b) > 0:
+        w.vars['iterations'] += 1
+        sleep(DELAY)
+        ca.select(ci + i)
+        if len(a) == 0:
+            c.append(b.pop(0))
+        elif len(b) == 0:
+            c.append(a.pop(0))
+        elif a[0] < b[0]:
+            c.append(a.pop(0))
+        else:
+            c.append(b.pop(0))
+        i += 1
+    return c
+
+def swap(w, a, b):
+    w.vars['swaps'] += 1
+    return b, a
+
+#not very good
 def magic_sort (w, array, ca):
     #title
     ca.text[0] = "Magic Sort"
 
-    ret_arr = [0]  
+    ret_arr = [0]
     for i in range (1,len (array)):
-        
+
         w.vars['iterations'] += 1
-        
+
         insert = len (ret_arr)
         for c in ret_arr:
-            sleep(0.0001)
+            sleep(DELAY)
             ca.select(c)
             if (array[i] > array[c]):
                 insert -= 1
         ret_arr.insert (insert, i)
-            
+
         ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
         ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
-    
+
     new_arr = []
     ca.select(-1)
     for i in ret_arr:
-        sleep(0.001)
+        sleep(DELAY)
         w.vars['swaps'] += 1
         w.vars['iterations'] += 1
         new_arr.insert(0, array[i])
         ca.update_array (new_arr)
         ca.text[1] = "Iterations: {}".format(w.vars['iterations'])
         ca.text[2] = "Swaps: {}".format(w.vars['swaps'])
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
